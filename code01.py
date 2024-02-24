c8f04ea0de6355095ca93b8458e42bdecfb8a5b6
Alexalen0 <alex3alen@gmail.com>
2024-02-24 11:06:45 +0530

    01_added\

---
+++ code01.py
@@ -0,0 +1,10 @@
+# py -m pip install opencv-python
+import cv2 as cv
+import sys
+img = cv.imread(cv.samples.findFile("ware_house.jpg"))
+if img is None:
+    sys.exit("Could not read the image.")
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
