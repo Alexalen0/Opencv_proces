937f04162e63aa99de060b725cfff7d413698380
Alexalen0 <alex3alen@gmail.com>
2024-02-24 11:41:32 +0530

    --img_color,gray_scale,unchanged

--- code01.py
+++ code01.py
@@ -1,10 +1,26 @@
 # py -m pip install opencv-python
 import cv2 as cv
+import numpy as np
 import sys
-img = cv.imread(cv.samples.findFile("ware_house.jpg"))
+
+img = cv.imread(cv.samples.findFile("rose.jpg"))
+img_color = cv.imread('rose.jpg', cv.IMREAD_COLOR)              #1
+img_grayscale = cv.imread('rose.jpg', cv.IMREAD_GRAYSCALE)      #0
+img_unchanged = cv.imread('rose.jpg', cv.IMREAD_UNCHANGED)      #-1
+
 if img is None:
     sys.exit("Could not read the image.")
+if img_grayscale is None:
+    sys.exit("Could not read the image.")
+if img_color is None:
+    sys.exit("Could not read the image.")
+if img_unchanged is None:
+    sys.exit("Could not read the image.")
+
 cv.imshow("Display window", img)
-k = cv.waitKey(0)
-if k == ord("s"):
-    cv.imwrite("starry_night.png", img)
+cv.imshow("img_color", img_color)
+cv.imshow("img_grayscale", img_grayscale)
+cv.imshow("img_unchanged", img_unchanged)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
